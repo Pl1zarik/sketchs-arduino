a4f45761b522a4604679f67059b63b50498ddc49
Pl1zarik <121376270+Pl1zarik@users.noreply.github.com>
2025-05-25 11:32:45 +0300

    Update адские муки.ino

--- адские муки.ino
+++ адские муки.ino
@@ -1,34 +1,51 @@
+#include "GyverEncoder.h"
 #include <Wire.h> 
 #include <iarduino_DHT.h>  
 #include <LiquidCrystal_I2C.h>  
-iarduino_DHT sensor(4);                                  
+
+iarduino_DHT sensor(4);    
+Encoder enc1(10, 11, 12);                              
 LiquidCrystal_I2C lcd(0x27,16,2); 
-void setup(){
-Serial.begin(9600);  
-lcd.init();                       
-lcd.backlight(); 
-lcd.scrollDisplayRight();
-lcd.setCursor(5,0);
-lcd.print("hello");
-delay(5000);
-lcd.clear();
-lcd.print("Temperature:");
-lcd.setCursor(0,1);
-lcd.print("Humidity:");
-}
-void loop(){
-info();
-
+int a = 0;
+int value = 0;
+void setup() {
+  Serial.begin(9600);  
+  lcd.init();  
+  enc1.setType(TYPE2);                     
+  lcd.backlight();
+  lcd.setCursor(5,0);
+  lcd.print("hello");
+  delay(2000);
+  lcd.clear();
 }
 
-void info(){
-sensor.read();
-lcd.setCursor(12,0);
-lcd.print(int(sensor.tem));
-lcd.setCursor(14,0);
-lcd.print("C");
-lcd.setCursor(9,1);
-lcd.print(int(sensor.hum));
-lcd.setCursor(11,1);
-lcd.print("%");
+void loop() {
+  lcd.setCursor(0,0);
+  // menu();
+  sensor.read();
+  info();
+  enc1.tick();
+  
+}
+
+void info() {
+  lcd.setCursor(0,0);
+  lcd.print("Temperature:");
+  lcd.setCursor(12,0);
+  lcd.print(int(sensor.tem));
+  lcd.print("C   ");
+  
+  lcd.setCursor(0,1);
+  lcd.print("Humidity:");
+  lcd.setCursor(9,1);
+  lcd.print(int(sensor.hum));
+  lcd.print("%   ");
+}
+
+void menu() {
+  lcd.setCursor(0,0);
+  lcd.print(" info");
+  lcd.setCursor(0,1);
+  lcd.print(" settings");
+
 }
